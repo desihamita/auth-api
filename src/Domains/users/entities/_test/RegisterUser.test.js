2c19a41e9824d11464f836e406dd199dc0ef3c50
desihamita <69859667+desihamita@users.noreply.github.com>
2023-09-16 13:58:21 +0700

    fixing registerUser

--- src/Domains/users/entities/_test/RegisterUser.test.js
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -11,4 +11,56 @@ describe('A RegisterUser entities', () => {
     // action and assert
     expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
   });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // arrage
+    const payload = {
+      username: 123,
+      fullname: true,
+      password: 'abc',
+    };
+
+    // action and assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should throw error when username contains more than 50 character', () => {
+    const payload = {
+      username: 'dicodingindonesiadicodingindonesiadicodingindonesiadicoding',
+      fullname: 'Dicoding Indonesia',
+      password: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_LIMIT_CHAR');
+  });
+
+  it('should throw error when username contains restricted character', () => {
+    // arrage
+    const payload = {
+      username: 'dico ding',
+      fullname: 'dicoding',
+      password: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+  });
+
+  it('should create registerUser object correctly', () => {
+    // Arrage
+    const payload = {
+      username: 'dicoding',
+      fullname: 'Dicoding Indonesia',
+      password: 'abc',
+    };
+
+    // Action
+    const { username, fullname, password } = new RegisterUser(payload);
+
+    // Assert
+    expect(username).toEqual(payload.username);
+    expect(fullname).toEqual(payload.fullname);
+    expect(password).toEqual(payload.password);
+  });
 });
